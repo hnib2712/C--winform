a0f4db0087917ef5ae7157d76cf1d461856ce4e5
hnibuv <54168622+hnibuv@users.noreply.github.com>
2020-09-20 21:45:36 +0700

    te
    C# đọc dữ liệu cổng com

---
+++ DocDuLieuCongCom/hnib/Form2.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace hnib
+{
+    partial class Hnibuv
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.TxtTL = new System.Windows.Forms.TextBox();
+            this.BtnKetnoi = new System.Windows.Forms.Button();
+            this.BtnNgatKN = new System.Windows.Forms.Button();
+            this.LbStatus = new System.Windows.Forms.Label();
+            this.CboCom = new System.Windows.Forms.ComboBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // TxtTL
+            // 
+            this.TxtTL.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.TxtTL.Font = new System.Drawing.Font("Modern No. 20", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtTL.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.TxtTL.Location = new System.Drawing.Point(161, 155);
+            this.TxtTL.Multiline = true;
+            this.TxtTL.Name = "TxtTL";
+            this.TxtTL.Size = new System.Drawing.Size(196, 74);
+            this.TxtTL.TabIndex = 0;
+            this.TxtTL.Text = "0";
+            this.TxtTL.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // BtnKetnoi
+            // 
+            this.BtnKetnoi.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnKetnoi.Location = new System.Drawing.Point(151, 79);
+            this.BtnKetnoi.Name = "BtnKetnoi";
+            this.BtnKetnoi.Size = new System.Drawing.Size(96, 32);
+            this.BtnKetnoi.TabIndex = 1;
+            this.BtnKetnoi.Text = "Kết nối";
+            this.BtnKetnoi.UseVisualStyleBackColor = true;
+            this.BtnKetnoi.Click += new System.EventHandler(this.BtnKetnoi_Click);
+            // 
+            // BtnNgatKN
+            // 
+            this.BtnNgatKN.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnNgatKN.Location = new System.Drawing.Point(253, 79);
+            this.BtnNgatKN.Name = "BtnNgatKN";
+            this.BtnNgatKN.Size = new System.Drawing.Size(121, 32);
+            this.BtnNgatKN.TabIndex = 1;
+            this.BtnNgatKN.Text = "Ngắt kết nối";
+            this.BtnNgatKN.UseVisualStyleBackColor = true;
+            this.BtnNgatKN.Click += new System.EventHandler(this.BtnNgatKN_Click);
+            // 
+            // LbStatus
+            // 
+            this.LbStatus.AutoSize = true;
+            this.LbStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LbStatus.ForeColor = System.Drawing.Color.Maroon;
+            this.LbStatus.Location = new System.Drawing.Point(306, 250);
+            this.LbStatus.Name = "LbStatus";
+            this.LbStatus.Size = new System.Drawing.Size(51, 16);
+            this.LbStatus.TabIndex = 2;
+            this.LbStatus.Text = "Status";
+            // 
+            // CboCom
+            // 
+            this.CboCom.FormattingEnabled = true;
+            this.CboCom.Location = new System.Drawing.Point(151, 24);
+            this.CboCom.Name = "CboCom";
+            this.CboCom.Size = new System.Drawing.Size(223, 21);
+            this.CboCom.TabIndex = 3;
+            this.CboCom.SelectedIndexChanged += new System.EventHandler(this.CboCom_SelectedIndexChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // Hnibuv
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(541, 297);
+            this.Controls.Add(this.CboCom);
+            this.Controls.Add(this.LbStatus);
+            this.Controls.Add(this.BtnNgatKN);
+            this.Controls.Add(this.BtnKetnoi);
+            this.Controls.Add(this.TxtTL);
+            this.Name = "Hnibuv";
+            this.Text = "Hnibuv";
+            this.Load += new System.EventHandler(this.Hnibuv_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxtTL;
+        private System.Windows.Forms.Button BtnKetnoi;
+        private System.Windows.Forms.Button BtnNgatKN;
+        private System.Windows.Forms.Label LbStatus;
+        private System.Windows.Forms.ComboBox CboCom;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ DocDuLieuCongCom/hnib/Form2.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace hnib
+{
+    public partial class Hnibuv : Form
+    {
+        SerialPort P = new SerialPort(); // Khai báo 1 Object SerialPort mới.
+        public Hnibuv()
+        {
+            InitializeComponent();
+            string[] Ports = SerialPort.GetPortNames();
+            P.DataReceived += new SerialDataReceivedEventHandler(port_DataReceived);
+            // Thêm toàn bộ các COM đã tìm được vào combox cbCom
+            CboCom.Items.AddRange(Ports); // Sử dụng AddRange thay vì dùng foreach
+            P.ReadTimeout = 500;
+        }
+        
+
+        private void CboCom_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (P.IsOpen)
+            {
+                P.Close(); // Nếu đang mở Port thì phải đóng lại
+            }
+            P.PortName = CboCom.SelectedItem.ToString(); // Gán PortName bằng COM đã chọn
+        }
+
+        private void BtnKetnoi_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                P.BaudRate = 9600;
+                P.DataBits = 8;
+                P.Parity = Parity.None;
+                P.StopBits = StopBits.One;
+                P.Handshake = Handshake.None;
+                //P.StopBits = (StopBits)Enum.Parse(typeof(StopBits), "1");//stop bit
+                //P.Parity = (Parity)Enum.Parse(typeof(Parity), "None");//Parity
+                P.RtsEnable = true;
+                P.DtrEnable = true;
+                P.Open();
+                LbStatus.Text = "Đang kết nối với cổng " + CboCom.SelectedItem.ToString();
+                LbStatus.ForeColor = Color.Red;
+                timer1.Interval = 100;
+                timer1.Enabled = true;
+                timer1.Start();
+            }
+            catch 
+            {
+                MessageBox.Show("Không kết nối được.", "Chúc may mắn lần sau", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void BtnNgatKN_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (P.IsOpen)
+                {
+                    P.Close();
+                    LbStatus.Text = "Đóng kết nối";
+                }
+            }
+            catch
+            {
+            }
+        }
+
+        string str,data = "";
+        private void port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            //// Nếu cổng com đóng thì không làm gì
+            //if (!P.IsOpen) return;
+            ////biến data lưu dữ liệu có trên đường truyền
+            //str = P.ReadExisting();
+            //byte[] bytes = Encoding.Default.GetBytes(str);
+            //data = Encoding.UTF8.GetString(bytes);
+            
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            // Nếu cổng com đóng thì không làm gì
+            if (!P.IsOpen) return;
+            //biến data lưu dữ liệu có trên đường truyền
+            str = P.ReadExisting();
+            byte[] bytes = Encoding.Default.GetBytes(str);
+            data = Encoding.UTF8.GetString(bytes);
+            if (data != String.Empty)
+            {
+                TxtTL.Text = data;
+            }
+        }
+
+        private void Hnibuv_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
